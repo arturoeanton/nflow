16fe76791a1b96e7444a9ee8f8c5cb1303d4392b
arturoeanton <arturoeanton@gmail.com>
2023-06-14 22:36:38 -0300

    remove external pluing suport

--- pkg/playbook/plugins.go
+++ pkg/playbook/plugins.go
@@ -1,9 +1,6 @@
 package playbook
 
 import (
-	"fmt"
-	"plugin"
-
 	"github.com/labstack/echo/v4"
 
 	"github.com/arturoeanton/nFlow/pkg/plugins"
@@ -34,43 +31,5 @@ func LoadPlugins() {
 
 	pluing5 := plugins.RulePlugin("rule")
 	Plugins[pluing5.Name()] = pluing5
-
-	for _, mod := range Config.PluginConfig.Plugins {
-
-		// load module
-		// 1. open the so file to load the symbols
-		func() {
-			defer func() {
-				if r := recover(); r != nil {
-					fmt.Println("Try use \n\tmake run")
-					fmt.Println("Recovered in f", r)
-				}
-			}()
-			plug, err := plugin.Open(mod)
-			if err != nil {
-				fmt.Println(err)
-				//os.Exit(1)
-			}
-
-			// 2. look up a symbol (an exported function or variable)
-			// in this case, variable NflowPlugin
-			symNflowPlugin, err := plug.Lookup("Dromedary")
-			if err != nil {
-				fmt.Println(err)
-				//os.Exit(1)
-			}
-
-			// 3. Assert that loaded symbol is of a desired type
-			// in this case interface type Dromedary (defined above)
-			var plugin NflowPlugin
-			plugin, ok := symNflowPlugin.(NflowPlugin)
-			if !ok {
-				fmt.Println("unexpected type from module symbol")
-				//os.Exit(1)
-			}
-			Plugins[plugin.Name()] = plugin
-			fmt.Println(plugin.Name())
-		}()
-	}
 
 }
