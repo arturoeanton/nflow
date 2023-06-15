2175ebbd427577884915def1ff78ec787142d811
arturoeanton <arturoeanton@gmail.com>
2023-06-14 22:37:17 -0300

    remove hopbox app

--- hopbox/modules/form_collector/js/code.js
+++
@@ -1,9 +0,0 @@
-function main(){
-  	var storage_id =  nflow_data["storage_id"] +"-"
-    if (nflow_data["storage_id"] == "") {
-      storage_id = ""
-    }
-  	for (var key in form){
-  		set_session("nflow_form",storage_id+key, form[key])
-    }
-}
