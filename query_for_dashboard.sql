1f890bd6d019e2b8e2301b398f7ff5f312e0ed0c
arturoeanton <arturoeanton@gmail.com>
2023-08-05 23:06:14 -0300

    add tracker to sqlite TODO: add postgres

---
+++ query_for_dashboard.sql
@@ -0,0 +1,45 @@
+--cantidad de veces que pasaron por la caja
+SELECT
+	box_id,
+	box_name,
+	COUNT(*)
+FROM
+	tracker
+GROUP BY
+	box_id,
+	box_name
+-- cantidad de veces que paso una session por cada caja
+SELECT
+	log_id,
+	box_id,
+	box_name,
+	COUNT(*)
+FROM
+	tracker
+GROUP BY
+	log_id,
+	box_id,
+	box_name
+    
+-- son la cantidad de veces que paso una session distinta por la caja
+SELECT
+t.box_id, t.box_name, count(*)
+FROM
+	(
+		SELECT
+			log_id,
+			box_id,
+			box_name
+		FROM
+			tracker 
+		GROUP BY
+			log_id,
+			box_id,
+			box_name
+	) t
+GROUP BY t.box_id, t.box_name
+;
+
+
+
+
